771b0b5084e91ef27dca9d0f89d0964e4cefb193
Ojediran Lawrence <damilareojediran3@gmail.com>
2022-08-04 19:08:01 +0100

    update

--- src/Components/Home/Navbar.jsx
+++ src/Components/Home/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../../Assets/4066031.png";
+import Logo from "../../Assets/3.png";
 
 function Navbar() {
   return (
